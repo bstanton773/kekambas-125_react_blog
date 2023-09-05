94b70c3f1355b5f2ec1d8c894bc4828978d4acde
Brian <bstanton773@gmail.com>
2023-09-05 12:00:57 -0500

    Basic building blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 
 
 export default function App() {
+    const name:string = 'Brian';
+    const isLoggedIn:boolean = true;
+
+    const posts: {id:number, title:string}[] = [
+        {id: 1, title: 'Happy Tuesday'},
+        {id: 2, title: 'How was your weekend?'},
+        {id: 3, title: 'I love React!'}
+    ];
+
     return (
-        <div>Hello World</div>
+        <div>
+            { isLoggedIn ? <h1>Hello {name}!</h1> : <h1>Hello and Welcome</h1>}
+            {posts.map(p => <li key={p.id}>{p.title}</li>)}
+        </div>
     )
 }
